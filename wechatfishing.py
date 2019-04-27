4b7110e02894ed32336ad1d696884ad2ae19a185
johnnyyee <johnnyyeyyy4210@gmail.com>
2019-04-27 16:37:05 +0200

    condensed the messaging and changed the formatting

--- wechatfishing.py
+++ wechatfishing.py
@@ -13,12 +13,14 @@ class reply:
     fish4 = "a giant squid! +5"
     fish5 = "a MEGALODON!!! +10"
     welcome = "Welcome to the fishing game!"
-    uc = "You caught"
+    uc = "You caught "
     
 class Turn:
     player1turn = True
     result = ""
     switch = False
+    message1 = "Player 1's turn"
+    message2 = "Player 2's turn"
 class Points:
     p1 = 0
     p2 = 0
@@ -41,10 +43,9 @@ def play_game(msg):
         Turn.switch = False
     if "fish" in msg.text:
         if Turn.player1turn:
-            msg.reply(f"Player 1's Turn, {Points.p1} points")
+            msg.reply(f"Player 1's Turn, {Points.p1} points\r"+str(fish()))
         else:
-            msg.reply(f"Player 2's Turn, {Points.p2} points")
-        msg.reply(fish())
+            msg.reply(f"Player 2's Turn, {Points.p2} points\r"+str(fish()))
     elif "stop" in msg.text:
         msg.reply("You ended your turn! It is the other player's turn.")
         Turn.switch = True
@@ -55,35 +56,35 @@ def fish():
     num = random.randint(1,6)
     
     if num == 1:
-        return (reply.uc, reply.fish1)
+        return str(reply.uc + reply.fish1)
     
     elif num == 2:
         if Turn.player1turn:
             Points.p1 += 1
         else:
             Points.p2 += 1
-        return (reply.uc, reply.fish2)
+        return str(reply.uc + reply.fish2)
         
     elif num == 3:
         if Turn.player1turn:
             Points.p1 += 3
         else:
             Points.p2 += 3
-        return (reply.uc, reply.fish3)
+        return str(reply.uc + reply.fish3)
     
     elif num == 4:
         if Turn.player1turn:
             Points.p1 += 5
         else:
             Points.p2 += 5
-        return (reply.uc, reply.fish4)
+        return str(reply.uc + reply.fish4)
     
     elif num == 5:
         if Turn.player1turn:
             Points.p1 += 10
         else:
             Points.p2 += 10
-        return (reply.uc, reply.fish5)
+        return str(reply.uc + reply.fish5)
     
     else:
         if Turn.player1turn:
@@ -91,7 +92,7 @@ def fish():
         else:
             Points.p2 = 0
         Turn.switch = True
-        return (reply.uc, "an electric eel! You have zero points and your Turn is over.")
+        return str(reply.uc + "an electric eel! You have zero points and your Turn is over.")
             
             
 embed()
